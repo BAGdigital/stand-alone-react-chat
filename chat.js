c4f334155631df250a9c3db23b433773145a90d7
N Balgopal Patro <n.balgopalpatra@gmail.com>
2021-07-31 12:39:59 +0530

    Room Chat done

--- chat.js
+++ chat.js
@@ -5,17 +5,66 @@ var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = [
 console.log('window', window);
 var e = React.createElement;
 var App = function App(props) {
+
     return React.createElement(
         'div',
         null,
         React.createElement(Button, null)
     );
 };
+// window.ReactBootstrap
 var Button = function Button(props) {
     var _React$useState = React.useState(false),
         _React$useState2 = _slicedToArray(_React$useState, 2),
         open = _React$useState2[0],
         setOpen = _React$useState2[1];
+
+    var _React$useState3 = React.useState(''),
+        _React$useState4 = _slicedToArray(_React$useState3, 2),
+        roomNo = _React$useState4[0],
+        setRoomNo = _React$useState4[1];
+
+    var _React$useState5 = React.useState([]),
+        _React$useState6 = _slicedToArray(_React$useState5, 2),
+        messages = _React$useState6[0],
+        setMessages = _React$useState6[1];
+
+    var _React$useState7 = React.useState(null),
+        _React$useState8 = _slicedToArray(_React$useState7, 2),
+        socket = _React$useState8[0],
+        setSocket = _React$useState8[1];
+
+    var _React$useState9 = React.useState(''),
+        _React$useState10 = _slicedToArray(_React$useState9, 2),
+        curmsg = _React$useState10[0],
+        setCurmsg = _React$useState10[1];
+
+    var _React$useState11 = React.useState(''),
+        _React$useState12 = _slicedToArray(_React$useState11, 2),
+        uname = _React$useState12[0],
+        setUname = _React$useState12[1];
+
+    var connectRoom = function connectRoom() {
+        // create a new socket connection using websocket
+        var chatSocket = new WebSocket('ws://' + 'winsor.in:8000' + '/ws/chat/' + roomNo + '/');
+        // set the socket to the state
+        setSocket(chatSocket);
+    };
+
+    React.useEffect(function () {
+        if (socket) {
+            socket.onmessage = function (e) {
+                var data = JSON.parse(e.data);
+                console.log(data);
+                document.getElementById("chat-log").innerHTML += data.message + '<br/>';
+                setMessages(function (old) {
+                    console.log(old);
+                    old.push(data);
+                    return old;
+                });
+            };
+        }
+    }, [socket]);
 
     return React.createElement(
         'div',
@@ -33,7 +82,51 @@ var Button = function Button(props) {
             React.createElement(
                 'div',
                 { className: 'chatWindow-header' },
-                'Chat Header'
+                'Chat Header',
+                React.createElement('div', { id: 'chat-log' }),
+                socket ? React.createElement(
+                    'div',
+                    null,
+                    React.createElement('input', { value: curmsg,
+                        onChange: function onChange(e) {
+                            setCurmsg(e.target.value);
+                        }
+                    }),
+                    React.createElement(
+                        'button',
+                        {
+                            onClick: function onClick() {
+                                socket.send(JSON.stringify({
+                                    'username': uname,
+                                    'message': curmsg
+                                }));
+                                setCurmsg('');
+                            }
+                        },
+                        'send'
+                    )
+                ) : React.createElement(
+                    'div',
+                    null,
+                    React.createElement('input', {
+                        placeholder: 'user name',
+                        value: uname,
+                        onChange: function onChange(e) {
+                            return setUname(e.target.value);
+                        }
+                    }),
+                    React.createElement('input', {
+                        value: roomNo,
+                        onChange: function onChange(e) {
+                            return setRoomNo(e.target.value);
+                        }
+                    }),
+                    React.createElement(
+                        'button',
+                        { onClick: connectRoom },
+                        'Go'
+                    )
+                )
             )
         )
     );
--- src/chat.js
+++ src/chat.js
@@ -2,18 +2,90 @@
 console.log('window', window);
 const e = React.createElement;
 const App = (props) => {
+
     return <div>
         <Button />
     </div>
 }
+// window.ReactBootstrap
 const Button = (props) => {
     const [open, setOpen] = React.useState(false);
+    const [roomNo, setRoomNo] = React.useState('');
+    const [messages, setMessages] = React.useState([]);
+    const [socket, setSocket] = React.useState(null);
+    const [curmsg, setCurmsg] = React.useState('');
+    const [uname, setUname] = React.useState('');
+    const connectRoom = () => {
+        // create a new socket connection using websocket
+        const chatSocket = new WebSocket(
+            'ws://'
+            + 'winsor.in:8000'
+            + '/ws/chat/'
+            + roomNo
+            + '/'
+        );
+        // set the socket to the state
+        setSocket(chatSocket);
+    }
+
+    React.useEffect(() => {
+        if (socket) {
+            socket.onmessage = function (e) {
+                const data = JSON.parse(e.data);
+                console.log(data);
+                document.getElementById("chat-log").innerHTML += data.message + '<br/>';
+                setMessages(old => {
+                    console.log(old)
+                    old.push(data)
+                    return old;
+                })
+            };
+        }
+    }, [socket]);
+
     return (
         <div>
             <button className="Chatbtn" onClick={() => { setOpen(!open) }}><i className="fas fa-comment-alt"></i></button>
             <div className={`chatwindow ${open ? '' : "hide"}`} >
                 <div className="chatWindow-header">
                     Chat Header
+                    <div id="chat-log"></div>
+                    {/* {messages.map((m, index) => {
+                        return <div key={index}>{m.message}</div>
+                    })} */}
+                    {
+                        socket ? <div>
+                            <input value={curmsg}
+                                onChange={e => {
+                                    setCurmsg(e.target.value)
+                                }}
+                            />
+                            <button
+                                onClick={() => {
+                                    socket.send(JSON.stringify({
+                                        'username': uname,
+                                        'message': curmsg
+                                    }))
+                                    setCurmsg('')
+                                }}
+                            >
+                                send
+                            </button>
+                        </div> : <div>
+                            <input
+                                placeholder="user name"
+                                value={uname}
+                                onChange={(e) => setUname(e.target.value)}
+                            />
+                            <input
+                                value={roomNo}
+                                onChange={(e) => setRoomNo(e.target.value)}
+                            />
+                            <button onClick={connectRoom} >Go</button>
+                        </div>
+                    }
+
+
                 </div>
             </div>
         </div>
